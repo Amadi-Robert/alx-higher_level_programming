f6fd4e29b4460ad5d23c6bb6f6cf4543f8e9cce7
Amadi-Robert <amadirobert18@gmail.com>
2023-03-09 03:07:55 -0800

    1-calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-if __name == "__main__":
+if __name__ == "__main__":
     """Function that adds, substructs, multiply, divides & return the value"""
     from calculator_1 import add, sub, mul, div
 
