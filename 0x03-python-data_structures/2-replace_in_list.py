d2c948a9802cd4586444667c956302abbb0980ba
Amadi-Robert <amadirobert18@gmail.com>
2023-03-11 17:13:39 -0800

    2-replace_in_list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Function that replaces an element of a list at a specific position"""
+
+
+def replace_in_list(my_list, idx, element):
+
+    if idx < 0 or >= len(my_list):
+        return None
+    else:
+        my_list[idx] = element
