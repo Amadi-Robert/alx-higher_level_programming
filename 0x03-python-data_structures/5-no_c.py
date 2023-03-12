5fc41c06cf1d288214ea4c22415f58950dd56bdc
Amadi-Robert <amadirobert18@gmail.com>
2023-03-12 16:57:17 -0700

    5_no_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    char_to_remove = ['c', 'C']
-    for char in char_to_remove:
-        while char in string:
-            my_string.remove(char)
-            return my_string
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
